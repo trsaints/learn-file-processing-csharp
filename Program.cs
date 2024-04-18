4d188616cde9f939f1fcfb7a9743704e6124132b
trsaints <thiago.dev2k3@gmail.com>
2024-04-18 04:55:47 -0300

    feat: add methods for listing files given multiple paths

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿var salesFiles = FindFiles("/home/trsaints/RiderProjects/mslearn-dotnet-files/stores/", "*.json", IsSalesFile);
+﻿var currentDirectory = Directory.GetCurrentDirectory();
 
+var salesFiles = FindFiles(currentDirectory, "*.json", IsSalesFile);
 
-foreach (var file in salesFiles)
-    Console.WriteLine(file);
+listFiles(salesFiles);
+
+var documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+salesFiles = FindFiles(documentsFolder, "*.json", IsSalesFile);
+
+listFiles(salesFiles);
+
+var customPath = Path.Combine("/home", "trsaints", "RiderProjects", "mslearn-dotnet-files", "stores");
+
+salesFiles = FindFiles(customPath, "*.json", IsSalesFile);
+
+listFiles(salesFiles);
+
 return;
 
 IEnumerable<string> FindFiles(string path, string fileExtension, Func<string, bool> filter)
@@ -18,3 +31,14 @@ bool IsSalesFile(string file)
     return file.EndsWith("sales.json");
 }
 
+void listFiles(IEnumerable<string> files)
+{
+    if (files.Count() == 0)
+    {
+        Console.WriteLine("No sales files found.");
+        return;
+    }
+
+    foreach (var file in files)
+        Console.WriteLine(file);
+}
