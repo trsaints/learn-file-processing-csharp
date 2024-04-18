0df2a04bdd485a42de70ec772d8a095463156a7b
trsaints <thiago.dev2k3@gmail.com>
2024-04-18 04:16:38 -0300

    feat: add file searching methods using System libraries

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿var salesFiles = FindFiles("/home/trsaints/RiderProjects/mslearn-dotnet-files/stores/", "*.json", IsSalesFile);
+
+
+foreach (var file in salesFiles)
+    Console.WriteLine(file);
+return;
+
+IEnumerable<string> FindFiles(string path, string fileExtension, Func<string, bool> filter)
+{
+    Console.WriteLine($"Searching for files in {path}...");
+    var foundFiles = Directory.EnumerateFiles(path, fileExtension, SearchOption.AllDirectories);
+
+    return foundFiles.Where(filter).ToList();
+}
+
+bool IsSalesFile(string file)
+{
+    return file.EndsWith("sales.json");
+}
+
