28bcf31dd1a652bc874fae1af0a9276845a3169d
trsaints <thiago.dev2k3@gmail.com>
2024-04-20 08:09:19 -0300

    feat: add new service for generating report files

---
+++ SalesService.cs
@@ -0,0 +1,36 @@
+namespace mslearn_dotnet_files;
+
+public static class SalesService
+{
+    private static double CalculateTotals(IEnumerable<string> salesFiles)
+    {
+        double salesTotal = 0;
+        
+        foreach (var file in salesFiles)
+        {
+            var salesJson = FileService.ReadFile([file]);
+
+            var data = JsonService.ReadJson(salesJson);
+            
+            salesTotal += data?.Total ?? 0;
+        }
+
+        return salesTotal;
+    }
+
+    public static void GenerateTotalsReport()
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var storesDirectory = Path.Combine(currentDirectory, "stores");
+        var salesTotalDirectory = Path.Combine(currentDirectory, "salesTotalDir");
+
+        if (!Path.Exists(salesTotalDirectory))
+            Directory.CreateDirectory(salesTotalDirectory);
+
+        var salesFiles = FileService.FindFiles(storesDirectory, "*.json", FileService.IsSalesFile);
+
+        var salesTotal = CalculateTotals(salesFiles);
+
+        FileService.AppendTo([salesTotalDirectory, "totals.txt"], $"{salesTotal}{Environment.NewLine}");
+    }
+}
