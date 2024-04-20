f50e331b19078de9c6e529ef46cea0bc41762e4f
trsaints <thiago.dev2k3@gmail.com>
2024-04-20 08:10:11 -0300

    update: change main flow for abstracting file report process

--- Program.cs
+++ Program.cs
@@ -1,25 +1,11 @@
-﻿using System.Globalization;
-using mslearn_dotnet_files;
+﻿using mslearn_dotnet_files;
 
-var currentDirectory = Directory.GetCurrentDirectory();
-var storesDirectory = Path.Combine(currentDirectory, "stores");
-var salesTotalDirectory = Path.Combine(currentDirectory, "salesTotalDir");
-
-if (!Path.Exists(salesTotalDirectory))
-    Directory.CreateDirectory(salesTotalDirectory);
-
-var salesFiles = FileService.FindFiles(storesDirectory, "*.json", FileService.IsSalesFile);
-FileService.ListFiles(salesFiles);
-
-var jsonFiles = FileService.FindFiles(storesDirectory, "*", JsonService.IsJson);
-FileService.ListFiles(jsonFiles);
-
-FileService.WriteFile([salesTotalDirectory, "sales.json"]);
-
-var salesJson = FileService.ReadFile(["stores", "201", "sales.json"]);
-var salesData = JsonService.ReadJson(salesJson);
-var totalString = salesData.Total.ToString(CultureInfo.CurrentCulture);
-
-FileService.WriteFile([salesTotalDirectory, "totals.txt"], totalString);
-
-Console.WriteLine(salesData.Total);
+try
+{
+    SalesService.GenerateTotalsReport();
+    Console.WriteLine("Operation finished successfully.");
+}
+catch (UnauthorizedAccessException ex)
+{
+    Console.WriteLine($"Error: the application does not have permission to access the file. {ex.Message}");
+}
